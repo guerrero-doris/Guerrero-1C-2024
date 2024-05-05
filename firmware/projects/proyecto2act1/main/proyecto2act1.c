101fa8550058f98aa58fd889bd382d518db2c434
guerrero-doris <guerrerodorismicaela@gmail.com>
2024-05-04 22:43:22 -0300

    subo ejercicio 2 y 1 juntos, pero ordenados

--- firmware/projects/proyecto2act1/main/proyecto2act1.c
+++ firmware/projects/proyecto2act1/main/proyecto2act1.c
@@ -2,7 +2,8 @@
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
+ * El código controla un sistema que mide distancias con un sensor ultrasónico HC-SR04 y muestra los resultados en un LCD.
+ * Además, enciende y apaga LEDs según la distancia medida. Dos botones (switches) controlan la activación y la retención de la medición.
  *
  * <a href="https://drive.google.com/...">Operation Example</a>
  *
@@ -15,11 +16,11 @@
  *
  * @section changelog Changelog
  *
- * |   Date	    | Description                                    |
+ * |   Date	 | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 12/05/2024 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author  Doris Micaela Guerrero (doris.guerrero@ingenieria.uner.edu.ar)
  *
  */
 
@@ -41,21 +42,24 @@
 #define CONFIG_BLINK_PERIOD 1000
 #define CONFIG_BLINK_PERIOD_1 50
 #define CONFIG_BLINK_PERIOD_medicion_US 1000000
-/*#define CONFIG_BLINK_PERIOD_LED_2 1500
-#define CONFIG_BLINK_PERIOD_LED_3 500*/
 bool start = true;
 bool hold = false;
+/*==================[parte retirada del ejercicio 1]==========================*/
+/*#define CONFIG_BLINK_PERIOD_LED_2 1500
+#define CONFIG_BLINK_PERIOD_LED_3 500*/
 
 /*==================[internal data definition]===============================*/
 TaskHandle_t tecla_task_handle = NULL;
 TaskHandle_t medicion_task_handle = NULL;
 /*==================[internal functions declaration]=========================*/
-
+/**
+ * @brief Tarea encargada de medir la distancia, espera una notificación para comenzar la medición.
+ */
 void distanciaTask(void *pvParameter){
     uint16_t distancia;
     while(1)
     {
-     ulTaskNotifyTake(pdTRUE, portMAX_DELAY); /* La tarea espera en este punto hasta recibir una notificación */
+     ulTaskNotifyTake(pdTRUE, portMAX_DELAY); 
         if(start)
         {
          distancia=HcSr04ReadDistanceInCentimeters(); 
@@ -91,22 +95,28 @@ void distanciaTask(void *pvParameter){
        
     }
 }
-
-
+/**
+ * @brief envía una notificación a la tarea de medición para iniciar una nueva medición periódicamente.
+ */
 void FuncTimerA(void* param){
     xTaskNotifyGive(medicion_task_handle); /* Envía una notificación a la tarea asociada a la medicion */
 }
 
-//funcion de interrupcion
+/**
+ * @brief cambia el valor de la variable global 
+ */
 void comienzar_programa(){
      start = !start;
 }
+/**
+ * @brief cambia el valor de la variable global 
+ */
 void hold_medicion(){
       hold=!hold;
 }
 
 
-
+/*==================[parte retirada del ejercicio 1]==========================*/
 /*void FuncTimerA(void* param){
     xTaskNotifyGive(tecla_task_handle);   Envía una notificación a la tarea asociada a la tecla 
 }*/
@@ -146,10 +156,12 @@ void app_main(void){
     TimerInit(&timer_medicion);
       /* Creación de tareas */
     xTaskCreate(&distanciaTask, "Distancia", 2048, NULL, 5, &medicion_task_handle);
+     /* Creación de interrupciones */
     SwitchActivInt(SWITCH_1, &comienzar_programa, NULL);
     SwitchActivInt(SWITCH_2, &hold_medicion, NULL);
+     /* Inicialización del conteo del timer */
     TimerStart(timer_medicion.timer);
-
+/*==================[parte retirada del ejercicio 1]==========================*/
  //  xTaskCreate(&switchTask, "teclas", 512, NULL, 5, &tecla_task_handle);
       /* Inicialización del conteo de timers */
  // TimerStart(timer_tecla.timer);
